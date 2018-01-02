8a3275f9deaa70faf92533bc70ad009fc7c6457d
Leo <391912829@qq.com>
2018-01-02 15:29:28 +0800

    Update user_main.c

--- user_main.c
+++ user_main.c
@@ -1,13 +1,12 @@
 #include "mylib.h"
-
+#include "XXXX.h"
 
 
 
 
 void main()
 {
-	
-	
-	
-	
+	am_pctouart_cmd am_pctouart_cmd_ = new am_pctouart_cmd();
+	byte[] byteArray = System.Text.Encoding.ASCII.GetBytes(am_pctouart_cmd_.set_gw_dev_light_temperature(bar_off.Value, bar_dec.Value, bar_rec.Value));
+	mycomm.WritePort(byteArray, 0, byteArray.Length);	
 }
